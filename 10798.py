7af6398032f61dd600a353442b0d817bf988c9e2
mooyah5 <qorgkssk123@gmail.com>
2022-04-27 23:36:42 +0900

    0427 WED

---
+++ 10798.py
@@ -0,0 +1,13 @@
+
+
+res = [[-1] * 15 for i in range(15)]
+
+for i in range(5):
+    S = list(input())
+    for j in range(len(S)):
+        res[i][j] = S[j]
+
+for j in range(15):
+    for i in range(15):
+        if res[i][j] != -1:
+            print(res[i][j], end='')
