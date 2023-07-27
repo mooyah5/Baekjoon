7699a7339625c908bffc647647e3e4a930620dec
백한나 <97646914+mooyah5@users.noreply.github.com>
2023-07-28 07:03:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,10 +1,10 @@
+import heapq
 def solution(k, score):
     answer = []
     arr = []
-    for i in range(len(score)):
-        arr.append(score[i])    # arr에 현재 숫자 추가
-        if k < len(arr):        # 단, arr에 담긴 수의 개수가 k를 초과하면
-            arr.sort()              # arr을 정렬하고
-            arr = arr[1:]           # 가장 작은 수를 제외해준다.
-        answer.append(min(arr))   # arr의 최소값 출력
+    for s in score:
+        heapq.heappush(arr, s)
+        if len(arr) > k:
+            heapq.heappop(arr)
+        answer.append(arr[0])
     return answer
